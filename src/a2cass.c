4e88df375ad904934a49e2d3ec11422fc94ea356
Christopher Alan Mosher <chris@mosher.mine.nu>
2012-02-06 18:41:14 -0500

    initial src dir

---
+++ src/a2cass.c
@@ -0,0 +1,15 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <binary-io.h>
+
+int main(int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
